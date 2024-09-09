4b1772780a54b08b9f83efa2cb3faf96de9babbd
joseph mwalai <josephmwalai@gmail.com>
2024-09-09 13:26:47 +0300

    fragments-navigation

--- app/src/main/java/com/example/testnavigation/homePageFragment.kt
+++ app/src/main/java/com/example/testnavigation/homePageFragment.kt
@@ -5,13 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class homePageFragment : Fragment() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        
-        return inflater.inflate(R.layout.fragment_home_page, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_home_page, container, false)
+
+        bottomNavigationView = view.findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.bottom_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.bottom_reels -> {
+                    replaceFragment(ReelsFragment())
+                    true
+                }
+                R.id.bottom_add ->{
+                    Toast.makeText(requireContext()
+                    ,"Upload Images and Videos", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.bottom_search -> {
+                    replaceFragment(SearchFragment())
+                    true
+                }
+                R.id.bottom_profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+        replaceFragment(HomeFragment())
+
+        return view
     }
+    private fun replaceFragment(fragment: Fragment) {
+        parentFragmentManager.beginTransaction().replace(R.id.frame_container, fragment)
+            .commit()
+    }
+
 }
